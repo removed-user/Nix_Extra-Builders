e1f66aa3d0c87480a07f0ee2e6f9739b5ad63c3e
casey <caseyriley94@gmail.com>
2026-05-15 03:04:36 -0400

    add function attrs to function, to pull in lib+flake-parts-lib

--- flakeModule.nix
+++ flakeModule.nix
@@ -1,5 +1,9 @@
 {
-  perSystem = {pkgs, ...}: {
+  perSystem = {
+    lib,
+    flake-parts-lib,
+    ...
+  }: {
     _module.args.Builders = {
       MkMakeBuilder = import ./Builders/MkMakeBuilder.nix;
       MkMesonPkg = import ./Builders/MkMesonPkg.nix;
