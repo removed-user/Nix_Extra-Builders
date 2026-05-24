6fa302a6f0194a557825ead1b9048cffe6286cc0
casey <caseyriley94@gmail.com>
2026-05-24 00:19:56 -0400

    rm test conf

--- flakeModule.nix
+++ flakeModule.nix
@@ -3,7 +3,7 @@
   # Import an importer file and pass it flake-parts-lib
   loadBuilders = lib.importApply ./lib/function_importer.nix {inherit lib;};
 in {
-  imports = [./config.nix];
+  # imports = [./config.nix];
 
   config.perSystem = {pkgs, ...}: {
     ExtraBuilders = loadBuilders {
