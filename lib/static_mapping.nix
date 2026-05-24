be924fbea280ed32afa273e69f10782c48a5d671
removed-user <caseyriley94@gmail.com>
2026-05-24 16:43:10 -0400

    Refactor static metadata map and submodule schema

--- lib/static_mapping.nix
+++ lib/static_mapping.nix
@@ -8,30 +8,42 @@ let
     (fileName: fileType: fileType == "regular" && lib.hasSuffix ".nix" fileName) 
     (builtins.readDir functionsDir); 
 
-  # 2. Pre-evaluated, fully populated static metadata map
+  # 2. Pre-evaluated, static metadata map
+  # In a literal attribute set, keys are flat names, not paths like `config.foo`
   staticPerFunctionOptionsSchema = lib.mapAttrs (fileName: _: 
     let 
       BuilderName = lib.removeSuffix ".nix" fileName;
       _scopedOptionDeclsFile = (toString OptionDeclsDir) + "/${BuilderName}.nix";
     in {
-      # We define these as static configurations that seed the submodule
- config.BuilderName = BuilderName;
- config._scopedOptionDeclsFile = _scopedOptionDeclsFile;
-config._hasScopedDecls = builtins.pathExists _scopedOptionDeclsFile;
+      # Seed values passed as a raw attribute set directly to the submodule default template
+      BuilderName = BuilderName;
+      _scopedOptionDeclsFile = _scopedOptionDeclsFile;
+      _hasScopedDecls = builtins.pathExists _scopedOptionDeclsFile;
       builderOptionsSchema = {}; 
     }
   ) discoveredNixFiles;
 
-  # 3. Submodule schema directly extracts the top-level pre-calculated matrix values
-  builderSubmodule = options = { 
+  # 3. Submodule schema correctly accepting config and options as arguments
+  builderSubmodule = { config, options, ... }: { 
+    options = { 
       BuilderName = lib.mkOption { 
         type = lib.types.str; 
-        default = config.BuilderName; # Pulls directly from pre-evaluated matrix
+        # Safely extracts the default value from the static metadata map seed
+        default = config.BuilderName; 
       }; 
 
       # Hidden structural internal options used for mapping
-      _scopedOptionDeclsFile = lib.mkOption { type = lib.types.str; internal = true; };
-      _hasScopedDecls = lib.mkOption { type = lib.types.bool; internal = true; };
+      _scopedOptionDeclsFile = lib.mkOption { 
+        type = lib.types.str; 
+        internal = true; 
+        default = config._scopedOptionDeclsFile; 
+      };
+      
+      _hasScopedDecls = lib.mkOption { 
+        type = lib.types.bool; 
+        internal = true; 
+        default = config._hasScopedDecls; 
+      };
 
       builderOutputModule = lib.mkOption { 
         type = lib.types.deferredModule; 
@@ -39,7 +51,8 @@ config._hasScopedDecls = builtins.pathExists _scopedOptionDeclsFile;
         
         default = { ... }: {
           options = {
-            builders.${options.BuilderName} = {
+            # Safely referencing the value via the validated options/config context
+            builders.${options.BuilderName.value} = {
               imports = [ defaultOptionDeclsFile ] 
                 ++ lib.optional config._hasScopedDecls config._scopedOptionDeclsFile; 
             };
