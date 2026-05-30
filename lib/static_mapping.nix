22a258347a8d9cf9be5852ae7d9eba7891d801c8
casey <caseyriley94@gmail.com>
2026-05-30 03:42:22 -0400

    Moved Reusable_functions to It's Own Repo - as was the goal.

--- lib/static_mapping.nix
+++ lib/static_mapping.nix
@@ -11,45 +11,40 @@
     (lib.readDir functionsDir);
 
   # 2. Pre-evaluated, static metadata map
-  MetaPerFunction = lib.mapAttrs (
-    fileName: _: let
-      OptionDeclsDir = ./OptionsDeclarations/PerFunction;
-      meta = {
-        defaultOptionDeclsFile = ./OptionsDeclarations/AllFunctions.nix;
-        perFunctionOptionDeclsFile = (toString OptionDeclsDir) + "/${fileName}";
-        BuilderName = lib.removeSuffix ".nix" fileName;
-      };
-    in {
-      # Seed values passed as an attribute set directly to the submodule generator function
-      metaForSubModuleBuilder = meta;
-    }
-  )
-  functionFiles;
+  MetaPerFunction =
+    lib.mapAttrs (
+      fileName: _: let
+        OptionDeclsDir = ./OptionsDeclarations/PerFunction;
+        metaForSubModuleBuilder = {
+          defaultOptionDeclsFile = ./OptionsDeclarations/AllFunctions.nix;
+          perFunctionOptionDeclsFile = (toString OptionDeclsDir) + "/${fileName}";
+          BuilderName = lib.removeSuffix ".nix" fileName;
+        };
+      in {
+        # Seed values passed as an attribute set directly to the submodule generator function
+
+        config.builders."${metaForSubModuleBuilder.BuilderName}" = {
+          meta = {
+            Scoped_schema = metaForSubModuleBuilder.perFunctionOptionDeclsFile;
+            __file = functionsDir / "${fileName}";
+          };
+        };
+        meta = metaForSubModuleBuilder;
+      }
+    )
+    functionFiles;
 
   # 3. Submodule schema factory
-  makeBuilderSubmodule = {metaForSubModuleBuilder, ...}: {
+  makeBuilderSubmodule = {
+    meta,
+    options,
+    ...
+  }: {
     # defaultOptionDeclsFile
     # perFunctionOptionDeclsFile
     # BuilderName
   };
-
-  builderSubmodule = {
-    config,
-    options,
-    ...
-  }: {
-    # builderOptionsSchema
-    options = {
-      default = {...}: {
-        # Safely referencing the value via the validated options/config context
-        builders.${BuilderName} = {
-          imports =
-            [defaultOptionDeclsFile]
-            ++ [scopedOptionDeclsFile];
-        };
-      };
-    };
-  };
+  # builderOptionsSchema
 in {
   # 4. Bind the default values directly to the static mapping
   options.builders = lib.mkOption {
